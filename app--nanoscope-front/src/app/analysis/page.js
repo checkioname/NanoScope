acc05d1814bed58641f0fbb2ca0007ab8fd9d45a
checkioname <lucasgargalhone@gmail.com>
2025-06-30 14:49:23 -0300

    Feat: atualizacoes UI nav bar

---
+++ app--nanoscope-front/src/app/analysis/page.js
@@ -0,0 +1,5 @@
+export default function AnalysisPage() {
+    return <main>
+        <h1>Hi</h1>
+    </main>
+}
--- app--nanoscope-front/src/app/components/navbar/navbar.js
+++ app--nanoscope-front/src/app/components/navbar/navbar.js
@@ -3,13 +3,13 @@ import { Search, Bell } from 'lucide-react';
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between bg-gray-800 px-6 py-3 rounded-xl shadow-md">
+    <nav className="flex items-center justify-between bg-gray-800 px-6 rounded-full shadow-md border border-gray-700">
       <div className="flex items-center space-x-6">
         <div className="text-xl font-bold text-orange-400/90">NanoScope</div>
-        <button className="px-3 py-1 rounded-md bg-gray-800 text-gray-300 hover:bg-blue-700 hover:text-white">X-rays</button>
-        <button className="text-gray-400 hover:text-white">My Appointments</button>
-        <button className="text-gray-400 hover:text-white">Help</button>
-        <button className="text-gray-400 hover:text-white">Contact</button>
+        <button className="p-4 m-0 rounded-full bg-gray-800 text-gray-300 hover:bg-gray-700 hover:text-white">X-rays</button>
+        <button className="p-4 m-0 text-gray-400 rounded-full hover:bg-gray-700 hover:text-white">Help</button>
+        <button className="p-4 m-0 text-gray-400 rounded-full hover:bg-gray-700 hover:text-white">My Appointments</button>
+        <button className="p-4 m-0 text-gray-400 rounded-full hover:bg-gray-700 hover:text-white">Contact</button>
       </div>
       <div className="flex items-center space-x-4">
         <button className="p-1 rounded-md hover:bg-gray-800"><Search size={20} /></button>
@@ -18,3 +18,4 @@ export default function Navbar() {
     </nav>
   );
 }
+
